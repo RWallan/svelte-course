2c8436bba7e1dd36505e604e21d1083d59cd71fc
RWallan <3am.richardwallan@gmail.com>
2024-10-30 10:54:09 -0300

    feat: Primary button component

---
+++ ebook-landing/frontend/src/lib/components/Button.svelte
@@ -0,0 +1,23 @@
+<script>
+  let { children, ...props } = $props();
+</script>
+
+<button {...props}>{@render children()}</button>
+
+<style>
+    button {
+        background-color: black;
+        color: whitesmoke;
+        padding: 20px 24px;
+        font-weight: normal;
+        font-size: 22px;
+        text-transform: uppercase;
+        transition: all 0.3s;
+        border: 1px solid whitesmoke;
+    }
+
+    button:hover {
+        background-color: whitesmoke;
+        color: black;
+    }
+</style>
