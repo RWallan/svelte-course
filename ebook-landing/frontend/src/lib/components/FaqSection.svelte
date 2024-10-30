1b86c2f996abd8dc80a5e46df44a31f6590e5730
RWallan <3am.richardwallan@gmail.com>
2024-10-30 12:39:30 -0300

    feat: faq section component structure

---
+++ ebook-landing/frontend/src/lib/components/FaqSection.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Button, FaqItem } from "$components";
+  import mastodonIcon from "$assets/icons/mastodon.svg";
+</script>
+
+<section class="faq-section">
+  <h2 class="mb-l">Frequently Asked Questions</h2>
+  <div class="faq-container">
+    <FaqItem></FaqItem>
+  </div>
+  <div class="additional-info">
+    <Button class="mt-s">BUY NOW</Button>
+    <p class="mt-m">Any of other questions?</p>
+    <a href="https://bolha.us/@rwallan">
+      <p>
+        Hit me up on Mastodon <img
+          src={mastodonIcon}
+          class="mastodon-icon"
+          alt=""
+        />
+      </p>
+    </a>
+  </div>
+</section>
