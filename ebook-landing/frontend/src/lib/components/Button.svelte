a044c3b19a7d509b411eea0667fd7b5f5998d8a2
RWallan <3am.richardwallan@gmail.com>
2024-11-04 23:47:46 -0300

    feat: backend request w/ stripe integration

--- ebook-landing/frontend/src/lib/components/Button.svelte
+++ ebook-landing/frontend/src/lib/components/Button.svelte
@@ -1,8 +1,34 @@
 <script>
+  import {loadStripe} from '@stripe/stripe-js'
+  import {PUBLIC_FRONTEND_URL, PUBLIC_STRIPE_KEY} from '$env/static/public';
+  import {goto} from '$app/navigation';
+
   let { children, ...props } = $props();
+
+  const onclick = async () => {
+    try {
+        const stripe = await loadStripe(PUBLIC_STRIPE_KEY)
+
+        const response = await fetch('http://localhost:8000/checkout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                success_url: `${PUBLIC_FRONTEND_URL}/checkout/success`,
+                cancel_url: `${PUBLIC_FRONTEND_URL}/checkout/failure`,
+            })
+        })
+
+        const data = await response.json()
+        await stripe.redirectToCheckout({sessionId: data.session_id})
+    } catch (err) {
+        goto('/checkout/failure')
+    }
+  }
 </script>
 
-<button {...props}>{@render children()}</button>
+<button {...props} {onclick}>{@render children()}</button>
 
 <style>
     button {
