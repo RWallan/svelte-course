86c6bec2c92c9a1d98b59ef3d1c70d0d22a1ed86
RWallan <3am.richardwallan@gmail.com>
2024-11-04 23:46:50 -0300

    feat: CORS middleware to frontend

--- ebook-landing/backend/backend/app.py
+++ ebook-landing/backend/backend/app.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from backend.schemas import CheckoutInput, SessionOutput
+from backend.settings import settings
 
 app = FastAPI()
+
+stripe.api_key = settings.STRIPE_API_KEY
+session = stripe.checkout.Session(api_key=settings.STRIPE_API_KEY)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:5173'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 
 @app.get('/health_check')
