966904ab9abb890f58302b84390799d2e26ddad8
RWallan <3am.richardwallan@gmail.com>
2024-11-07 16:15:39 -0300

    feat: hero section structure

---
+++ dev-portfolio/frontend/src/lib/components/HeroSection.svelte
@@ -0,0 +1,14 @@
+<section class="hero-section default-margin">
+  <h3>Hello, I'm Richard</h3>
+  <div class="underscore mb-m"></div>
+  <h1>I'm a Data Scientist uncovering insights and building</h1>
+  <h1 class="dark-red">data-driven solutions.</h1>
+
+  <button>Let's Talk!</button>
+</section>
+
+<style>
+  .hero-section {
+    padding-top: 60px;
+  }
+</style>
