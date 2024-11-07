a9b222839d6260c52a854f1f88f8b1a44f0357fa
RWallan <3am.richardwallan@gmail.com>
2024-11-07 16:31:49 -0300

    feat: button styling

--- dev-portfolio/frontend/src/lib/components/Button.svelte
+++ dev-portfolio/frontend/src/lib/components/Button.svelte
@@ -13,3 +13,15 @@
 <button class={`btn ${className}`} {...props}>
     {@render children()}
 </button>
+
+<style>
+  .btn {
+    font-family: "Lato", sans-serif;
+    font-weight: 500;
+    background-color: #242d4e;
+    color: whitesmoke;
+    font-size: 24px;
+    padding: 14px 40px;
+    border-radius: 10px;
+  }
+</style>
