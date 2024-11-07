90064fe767bf235676b2a9c002e24aeb0c36a842
RWallan <3am.richardwallan@gmail.com>
2024-11-07 17:16:26 -0300

    feat: styling

--- dev-portfolio/frontend/src/lib/components/ExperienceTable.svelte
+++ dev-portfolio/frontend/src/lib/components/ExperienceTable.svelte
@@ -27,7 +27,7 @@
   ];
 </script>
 
-<section class="default-margin work-experience-mt-m">
+<section class="default-margin work-experience mt-m">
   <ul class="work-experience-list">
     {#each workExperience as job}
       <li class="work-item">
@@ -50,3 +50,37 @@
   </ul>
   <h2 class="headline">Past Work <br /> Experience</h2>
 </section>
+
+<style>
+  .work-experience {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  .work-experience-list {
+    width: 50%;
+  }
+
+  .headline {
+    text-align: right;
+  }
+
+  .work-item {
+    border-bottom: 1px solid #f0eded;
+    padding-bottom: 12px;
+  }
+
+  .work-item:not(:first-of-type) {
+    padding-top: 16px;
+  }
+
+  .work-item p {
+    margin-bottom: 0;
+  }
+
+  .company-and-date {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
