75e4c41bc1fc24bd87e0e7176ecd3a1bea2caa03
RWallan <3am.richardwallan@gmail.com>
2024-11-07 16:28:08 -0300

    feat: cta button

--- dev-portfolio/frontend/src/lib/components/Sections/HeroSection.svelte
+++ dev-portfolio/frontend/src/lib/components/Sections/HeroSection.svelte
@@ -1,10 +1,19 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { Button } from "$components";
+</script>
+
 <section class="hero-section default-margin">
   <h3>Hello, I'm Richard</h3>
   <div class="underscore mb-m"></div>
   <h1>I'm a Data Scientist uncovering insights and building</h1>
   <h1 class="dark-red">data-driven solutions.</h1>
 
-  <button>Let's Talk!</button>
+  <Button
+    onclick={() => {
+      goto("/#contact-form");
+    }}>Let's Talk!</Button
+  >
 </section>
 
 <style>
