052704e1631d44af70d82ca8cc540e383e2479be
RWallan <3am.richardwallan@gmail.com>
2024-11-07 16:40:41 -0300

    feat: section headline component

---
+++ dev-portfolio/frontend/src/lib/components/SectionHeadline.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  interface HeadlineProps {
+    children: Snippet;
+    sectionName: string;
+  }
+
+  let { children, sectionName }: HeadlineProps = $props();
+</script>
+
+<div class="default-margin" id={sectionName}>
+  <h2 >{@render children()}</h2>
+  <div class="underscore mb-m"></div>
+</div>
--- dev-portfolio/frontend/src/lib/components/index.ts
+++ dev-portfolio/frontend/src/lib/components/index.ts
@@ -1,2 +1,3 @@
 export { default as HeroSection } from "./Sections/HeroSection.svelte";
 export { default as Button } from "./Button.svelte";
+export { default as SectionHeadline } from "./SectionHeadline.svelte";
