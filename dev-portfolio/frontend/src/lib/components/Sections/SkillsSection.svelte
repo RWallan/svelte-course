30ef03721f2c8d0d64ae143a8840f46bd3c1a214
RWallan <3am.richardwallan@gmail.com>
2024-11-09 01:28:22 -0300

    feat: skills section styling

--- dev-portfolio/frontend/src/lib/components/Sections/SkillsSection.svelte
+++ dev-portfolio/frontend/src/lib/components/Sections/SkillsSection.svelte
@@ -19,3 +19,25 @@
     </div>
   </div>
 </section>
+
+<style>
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .skills-container {
+    width: 40%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    row-gap: 16px;
+    column-gap: 10px;
+  }
+
+  i {
+    font-size: 80px;
+    color: black;
+  }
+</style>
