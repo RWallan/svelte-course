203441b4ad0c0be17953d69215faf9cd738ec572
RWallan <3am.richardwallan@gmail.com>
2024-11-09 00:16:29 -0300

    feat: work section styling

--- dev-portfolio/frontend/src/lib/components/Sections/MyWorkSection.svelte
+++ dev-portfolio/frontend/src/lib/components/Sections/MyWorkSection.svelte
@@ -12,7 +12,7 @@
 <section class="mt-l">
   <SectionHeadline sectionName="my-work">My Work</SectionHeadline>
   <div class="default-margin projects-container mt-m">
-    <article class="main-project mb-m">
+    <article class="main-project-container mb-m">
       <a href={`/work/${mainProject.slug}`}>
         <img src={mainProject.projectImageUrl} alt={mainProject.name} />
       </a>
@@ -21,7 +21,7 @@
           <h3 class="semi-bold">{mainProject.name}</h3>
           <p class="company dark-grey">{mainProject.company}</p>
         </div>
-        <a href={`/work/${mainProject.slug}`} class="btn-to-article">→</a>
+        <a href={`/work/${mainProject.slug}`} class="btn-to-article">⟶</a>
       </div>
     </article>
     <div class="more-projects-container">
@@ -35,10 +35,56 @@
               <h3 class="semi-bold">{project.name}</h3>
               <p class="company dark-grey">{project.company}</p>
             </div>
-            <a href={`/work/${project.slug}`} class="btn-to-article">→</a>
+            <a href={`/work/${project.slug}`} class="btn-to-article">⟶</a>
           </div>
         </article>
       {/each}
     </div>
   </div>
 </section>
+
+<style>
+  img {
+    width: 100%;
+    border-radius: 20px;
+    object-fit: cover;
+    cursor: pointer;
+  }
+
+  .more-projects-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  .more-projects-container img {
+    height: 350px;
+  }
+
+  .main-project-container img {
+    max-height: 450px;
+  }
+
+  .project {
+    width: 48%;
+  }
+
+  .project-info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .btn-to-article {
+    display: block;
+    font-size: 40px;
+    border: 1px solid black;
+    border-radius: 40px;
+    padding: 0px 24px;
+  }
+
+  .company {
+    font-size: 18px;
+    margin-bottom: 0;
+  }
+</style>
