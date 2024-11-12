79b2b9c356ecf53db2ef37a1f64837c6083c75c9
RWallan <3am.richardwallan@gmail.com>
2024-11-11 21:08:50 -0300

    update: moving api url to env

--- ebook-landing/frontend/src/lib/components/Button.svelte
+++ ebook-landing/frontend/src/lib/components/Button.svelte
@@ -1,49 +1,53 @@
 <script>
-  import {loadStripe} from '@stripe/stripe-js'
-  import {PUBLIC_FRONTEND_URL, PUBLIC_STRIPE_KEY} from '$env/static/public';
-  import {goto} from '$app/navigation';
+  import { loadStripe } from "@stripe/stripe-js";
+  import {
+    PUBLIC_FRONTEND_URL,
+    PUBLIC_STRIPE_KEY,
+    PUBLIC_API_URL,
+  } from "$env/static/public";
+  import { goto } from "$app/navigation";
 
   let { children, ...props } = $props();
 
   const onclick = async () => {
     try {
-        const stripe = await loadStripe(PUBLIC_STRIPE_KEY)
+      const stripe = await loadStripe(PUBLIC_STRIPE_KEY);
 
-        const response = await fetch('http://localhost:8000/checkout', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                success_url: `${PUBLIC_FRONTEND_URL}/checkout/success`,
-                cancel_url: `${PUBLIC_FRONTEND_URL}/checkout/failure`,
-            })
-        })
+      const response = await fetch(`${PUBLIC_API_URL}/checkout`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          success_url: `${PUBLIC_FRONTEND_URL}/checkout/success`,
+          cancel_url: `${PUBLIC_FRONTEND_URL}/checkout/failure`,
+        }),
+      });
 
-        const data = await response.json()
-        await stripe.redirectToCheckout({sessionId: data.session_id})
+      const data = await response.json();
+      await stripe.redirectToCheckout({ sessionId: data.session_id });
     } catch (err) {
-        goto('/checkout/failure')
+      goto("/checkout/failure");
     }
-  }
+  };
 </script>
 
 <button {...props} {onclick}>{@render children()}</button>
 
 <style>
-    button {
-        background-color: black;
-        color: whitesmoke;
-        padding: 20px 24px;
-        font-weight: normal;
-        font-size: 22px;
-        text-transform: uppercase;
-        transition: all 0.3s;
-        border: 1px solid whitesmoke;
-    }
+  button {
+    background-color: black;
+    color: whitesmoke;
+    padding: 20px 24px;
+    font-weight: normal;
+    font-size: 22px;
+    text-transform: uppercase;
+    transition: all 0.3s;
+    border: 1px solid whitesmoke;
+  }
 
-    button:hover {
-        background-color: whitesmoke;
-        color: black;
-    }
+  button:hover {
+    background-color: whitesmoke;
+    color: black;
+  }
 </style>
