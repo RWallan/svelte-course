6c2a9e89d5c881c4b87a74a456a0856913dc053b
RWallan <3am.richardwallan@gmail.com>
2024-11-13 21:03:51 -0300

    feat: Header html structure

---
+++ book-nest/frontend/src/lib/components/layout/Header.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import logo from "$assets/app-logo.svg";
+  import { Button } from "$components";
+</script>
+
+<header>
+  <a href="/">
+    <img src={logo} alt="Go to home" class="logo" />
+  </a>
+  <nav>
+    <ul>
+      <li><Button isMenu={true} href="/register">Create account</Button></li>
+      <li>
+        <Button isMenu={true} isSecondary={true} href="/login">Login</Button>
+      </li>
+    </ul>
+  </nav>
+</header>
