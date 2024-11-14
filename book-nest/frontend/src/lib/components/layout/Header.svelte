28a2f168796f9fe4839e7e96e7ffb45c8f2a6293
RWallan <3am.richardwallan@gmail.com>
2024-11-13 21:06:00 -0300

    feat: header styling

--- book-nest/frontend/src/lib/components/layout/Header.svelte
+++ book-nest/frontend/src/lib/components/layout/Header.svelte
@@ -16,3 +16,21 @@
     </ul>
   </nav>
 </header>
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 12px 4vw;
+  }
+
+  .logo {
+    height: 72px;
+  }
+
+  ul {
+    display: flex;
+    column-gap: 24px;
+  }
+</style>
