aa75a6f87621c3514339c31f8e62bc46bb2caf82
RWallan <3am.richardwallan@gmail.com>
2024-11-25 11:15:31 -0300

    feat: sidebar html

---
+++ book-nest/frontend/src/routes/private/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { SideNavigation } from "$components";
+
+  let { children } = $props();
+</script>
+
+<div class="auth-area">
+  <SideNavigation />
+  <div class="main-area">
+    {@render children()}
+  </div>
+</div>
