b50b03706de8d18323a7969a117e3cb09100ce45
RWallan <3am.richardwallan@gmail.com>
2025-03-01 12:25:09 -0300

    feat: book category component

--- book-nest/src/lib/components/index.ts
+++ book-nest/src/lib/components/index.ts
@@ -3,5 +3,6 @@ export { default as Button } from './Button.svelte';
 export { default as AuthForm } from './AuthForm.svelte';
 export { default as BookCard } from './BookCard.svelte';
 export { default as StarRating } from './StarRating.svelte';
+export { default as BookCategory } from './BookCategory.svelte';
 export { default as Header } from './layout/Header.svelte';
 export { default as SideBar } from './layout/SideBar.svelte';
