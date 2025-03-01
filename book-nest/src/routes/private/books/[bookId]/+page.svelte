2c7d2417920a26147e6ddaf1c37fd78c7882d467
RWallan <3am.richardwallan@gmail.com>
2025-03-01 14:18:28 -0300

    feat: edit mode

--- book-nest/src/routes/private/books/[bookId]/+page.svelte
+++ book-nest/src/routes/private/books/[bookId]/+page.svelte
@@ -9,11 +9,24 @@
 		};
 	}
 	let { data }: BookPageProps = $props();
-
 	let { book } = $derived(data);
+	let isEditMode = $state(false);
+
+	// svelte-ignore state_referenced_locally
+	let title = $state(book.title);
+	// svelte-ignore state_referenced_locally
+	let author = $state(book.author);
+	// svelte-ignore state_referenced_locally
+	let description = $state(book.description || '');
+	// svelte-ignore state_referenced_locally
+	let genre = $state(book.genre || '');
 
 	const goBack = () => {
 		history.back();
+	};
+
+	const toogleEditMode = () => {
+		isEditMode = !isEditMode;
 	};
 </script>
 
@@ -43,13 +56,47 @@
 	{/if}
 {/snippet}
 
+{#snippet editFields()}
+	<form>
+		<input type="text" class="input input-title mt-m mb-xs" bind:value={title} name="title" />
+		<div class="input-author">
+			<p>by</p>
+			<input type="text" class="input" bind:value={author} name="title" />
+		</div>
+		<h4 class="mt-m mb-xs semi-bold">Your rating</h4>
+		<StarRating value={book.rating || 0} />
+		<p class="small-font">Click to {book.rating ? 'change' : 'give'} rating</p>
+		<h4 class="mt-m mb-xs semi-bold">Description</h4>
+		<textarea name="description" class="textarea mb-m" placeholder="Add a description"></textarea>
+		{#if !book.finished_reading_on}
+			<Button isSecondary={true} onclick={() => console.log('updating reading status')}>
+				{book.started_reading_on ? 'I finished reading this book' : 'I started reading this book'}
+			</Button>
+		{/if}
+		<h4 class="mt-m mb-xs semi-bold">Genre</h4>
+		<input class="input" bind:value={genre} type="text" name="genre" />
+	</form>
+{/snippet}
+
 <div class="book-page">
 	<button onclick={goBack} aria-label="Go back">
 		<Icon icon="ep:back" width={'40'} />
 	</button>
 	<div class="book-container">
 		<div class="book-info">
-			{@render bookInfo()}
+			{#if isEditMode}
+				{@render editFields()}
+			{:else}
+				{@render bookInfo()}
+			{/if}
+			<div class="button-container mt-m">
+				<Button isSecondary={true} onclick={toogleEditMode}
+					>{isEditMode ? 'Save changes' : 'Edit'}</Button
+				>
+				<Button isDanger={true} onclick={() => console.log('delete the book')}
+					>Delete book from library</Button
+				>
+			</div>
 		</div>
 		<div class="book-cover">
 			{#if book.cover_image}
@@ -82,7 +129,7 @@
 		border: 1px solid black;
 		border-radius: 15px;
 		min-height: 400px;
-		max-width: 350px;
+		max-width: 450px;
 		margin-left: 80px;
 	}
 
@@ -98,5 +145,29 @@
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
+	}
+
+	.input {
+		padding: 8px 4px;
+		width: 100%;
+	}
+
+	.textarea {
+		width: 100%;
+	}
+
+	.input-title {
+		font-size: 60px;
+		font-weight: bold;
+		font-family: 'EB Garamond', serif;
+	}
+
+	.input-author {
+		display: flex;
+		align-items: center;
+	}
+
+	.input-author p {
+		margin-right: 8px;
 	}
 </style>
