7728a5f25278f1687ab5d510dacb52e95e474e5a
RWallan <3am.richardwallan@gmail.com>
2025-02-28 22:23:51 -0300

    feat: logout

--- book-nest/src/lib/components/layout/Header.svelte
+++ book-nest/src/lib/components/layout/Header.svelte
@@ -1,19 +1,35 @@
 <script lang="ts">
 	import bookNestLogo from '$assets/app-logo.svg';
 	import { Button } from '$components';
+	import { getUserState } from '$components/state/user-state.svelte';
+
+	let userContext = getUserState();
+
+	let { user } = $derived(userContext);
 </script>
 
 <header>
 	<a href="/"><img src={bookNestLogo} alt="Go to home" class="logo" /></a>
 	<nav>
-		<ul>
-			<li>
-				<Button isMenu={true} href="/register">Create Account</Button>
-			</li>
-			<li>
-				<Button isMenu={true} isSecondary={true} href="/login">Login</Button>
-			</li>
-		</ul>
+		{#if !user}
+			<ul>
+				<li>
+					<Button isMenu={true} href="/register">Create Account</Button>
+				</li>
+				<li>
+					<Button isMenu={true} isSecondary={true} href="/login">Login</Button>
+				</li>
+			</ul>
+		{:else}
+			<ul>
+				<li>
+					{user.email}
+				</li>
+				<li>
+					<Button isMenu={true} onclick={() => userContext.logout()}>Logout</Button>
+				</li>
+			</ul>
+		{/if}
 	</nav>
 </header>
 
@@ -27,6 +43,7 @@
 
 	ul {
 		display: flex;
+		align-items: center;
 		column-gap: 24px;
 	}
 
