d2322c36bcd8acaee65a851b058a3adc8f652264
RWallan <3am.richardwallan@gmail.com>
2025-03-01 12:50:55 -0300

    fix: updating user state properly

--- book-nest/src/routes/+layout.svelte
+++ book-nest/src/routes/+layout.svelte
@@ -11,6 +11,7 @@
 
 	$effect(() => {
 		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			userState.updateState({ session: newSession, supabase, user: newSession?.user || null });
 			if (newSession?.expires_at !== session?.expires_at) {
 				invalidate('supabase:auth');
 			}
