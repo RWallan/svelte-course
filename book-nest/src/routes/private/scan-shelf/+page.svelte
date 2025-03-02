1c90a731d029b775b85a287dd459dae0e33f4557
RWallan <3am.richardwallan@gmail.com>
2025-03-02 13:00:52 -0300

    feat: scan shelf dropzone

---
+++ book-nest/src/routes/private/scan-shelf/+page.svelte
@@ -0,0 +1,16 @@
+
+<h2 class="mt-m mb-l">Take a picture to add books</h2>
+<div class="upload-area">
+	<div class="upload-container">
+		<Dropzone
+			on:drop={() => {}}
+			multiple={false}
+			accept="image/*"
+			maxSize={10 * 1024 * 1024}
+			containerClasses={'dropzone-cover'}
+		>
+			<Icon icon="bi:camera-fill" width={'40'} />
+			<p>Drag a picture here or click to select a file.</p>
+		</Dropzone>
+	</div>
+</div>
