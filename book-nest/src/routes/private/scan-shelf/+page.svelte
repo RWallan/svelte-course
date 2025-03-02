9f35263e5e47637a2033eb211c36384fbe832a78
RWallan <3am.richardwallan@gmail.com>
2025-03-02 13:43:59 -0300

    feat: scan shelf book list and buttons

--- book-nest/src/routes/private/scan-shelf/+page.svelte
+++ book-nest/src/routes/private/scan-shelf/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import { Button } from '$components';
 	import { convertFileToBase64 } from '$lib/utils/openai-helpers';
 	import Icon from '@iconify/svelte';
 	import Dropzone from 'svelte-file-dropzone';
 
 	let isLoading = $state(false);
+	let errorMessage = $state('');
+	let recognizedBooks = $state<OpenAIBook[]>([]);
+	let bookSuccessfullyAdded = $state(false);
 
 	interface OpenAIBook {
 		author: string;
@@ -26,26 +30,87 @@
 					body: JSON.stringify({ base64: base64String })
 				});
 
+				isLoading = false;
 				const result = (await response.json()) as { bookArray: OpenAIBook[] };
+				recognizedBooks = result.bookArray;
 			} catch (error) {
 				console.log('Some error happened while scanning the book', error);
+				errorMessage = 'Error processing the upload file';
 			}
+		} else {
+			errorMessage = `Could not upload given file. Are you sure it's an image with a file size less than 10MB?`;
 		}
 	};
 </script>
 
-<h2 class="mt-m mb-l">Take a picture to add books</h2>
-<div class="upload-area">
-	<div class="upload-container">
-		<Dropzone
-			on:drop={handleDrop}
-			multiple={false}
-			accept="image/*"
-			maxSize={10 * 1024 * 1024}
-			containerClasses={'dropzone-cover'}
-		>
-			<Icon icon="bi:camera-fill" width={'40'} />
-			<p>Drag a picture here or click to select a file.</p>
-		</Dropzone>
+{#snippet uploadArea()}
+	<div class="upload-area">
+		<div class="upload-container">
+			{#if errorMessage}
+				<h4 class="text-center mb-s upload-error">
+					{errorMessage}
+				</h4>
+			{/if}
+			{#if isLoading}
+				<div class="spinner-container">
+					<div class="spinner">
+						<p>Processing your books</p>
+					</div>
+				</div>
+			{:else}
+				<Dropzone
+					on:drop={handleDrop}
+					multiple={false}
+					accept="image/*"
+					maxSize={10 * 1024 * 1024}
+					containerClasses={'dropzone-cover'}
+				>
+					<Icon icon="bi:camera-fill" width={'40'} />
+					<p>Drag a picture here or click to select a file.</p>
+				</Dropzone>
+			{/if}
+		</div>
 	</div>
-</div>
+{/snippet}
+{#snippet foundBooks()}
+	<div class="found-books">
+		<table class="book-list mb-m">
+			<thead>
+				<tr>
+					<th>Title</th>
+					<th>Author</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each recognizedBooks as book, i}
+					<tr>
+						<td>{book.bookTitle}</td>
+						<td>{book.author}</td>
+						<td>
+							<button
+								type="button"
+								aria-label="Remove book"
+								class="remove-book"
+								onclick={() => console.log(`Delete book from index ${i}`)}
+							>
+								<Icon icon="streamline:delete-1-solid" width={'24'} />
+							</button>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+		<Button onclick={() => console.log('add all remaining books')}>Add all books</Button>
+	</div>
+{/snippet}
+
+<h2 class="mt-m mb-l">Take a picture to add books</h2>
+{#if recognizedBooks.length === 0}
+	{@render uploadArea()}
+{:else if !bookSuccessfullyAdded}
+	{@render foundBooks()}
+{:else}
+	<h4>The selected {recognizedBooks.length} books have been added to your library.</h4>
+	<Button href="/private/dashboard">Got to your library</Button>
+{/if}
